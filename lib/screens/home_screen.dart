cf386bec0734fabd5c5b1e87b52fb7978ed582f5
walgoguma <90331768+walgoguma@users.noreply.github.com>
2023-08-18 12:57:37 +0900

    #4.1 Timer
    버튼을 누르면 초가 움직이도록 동작

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  late Timer timer;
+
+  //초에 관한 변수값을 1씩 q뺌
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds = totalSeconds - 1;
+    });
+  }
+
+  //1초 마다 onTick 함수(초에 관한 변수값을 1씩 q뺌) 실행
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +39,7 @@ class HomeScreenState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                '25:00',
+                '$totalSeconds',
                 style: TextStyle(
                     color: Theme.of(context).cardColor,
                     fontSize: 89,
@@ -33,7 +53,7 @@ class HomeScreenState extends State<HomeScreen> {
                 child: IconButton(
               iconSize: 120,
               color: Theme.of(context).cardColor,
-              onPressed: () {},
+              onPressed: onStartPressed,
               icon: const Icon(
                 Icons.play_circle_outline,
               ),
@@ -47,6 +67,7 @@ class HomeScreenState extends State<HomeScreen> {
                   child: Container(
                     decoration: BoxDecoration(
                       color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(50),
                     ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
